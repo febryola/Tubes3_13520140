359eec700e2a1a7197f868c96b69ce580516392d
febryola <febryola0402@gmail.com>
2022-04-29 17:30:24 +0700

    update laporan

--- src/backend/matcher.js
+++ src/backend/matcher.js
@@ -12,11 +12,11 @@
  *                  is not found
  */
 function match(text, pattern) {
-  if (pattern.length > 50 || pattern.length > text.length / 2) {
-    return bmMatch(text, pattern);
-  } else {
-    return kmpMatch(text, pattern);
-  }
+    if (pattern.length > 50 || pattern.length > text.length / 2) {
+        return bmMatch(text, pattern);
+    } else {
+        return kmpMatch(text, pattern);
+    }
 }
 
 /**
@@ -29,27 +29,27 @@ function match(text, pattern) {
  *                  is not found
  */
 function kmpMatch(text, pattern) {
-  let n = text.length;
-  let m = pattern.length;
-  let border = calcKmpBorder(pattern);
+    let n = text.length;
+    let m = pattern.length;
+    let border = calcKmpBorder(pattern);
 
-  let i = 0;
-  let j = 0;
+    let i = 0;
+    let j = 0;
 
-  while (i < n) {
-    if (text[i] == pattern[j]) {
-      if (j == m - 1) {
-        return i - m + 1;
-      }
-      i++;
-      j++;
-    } else if (j > 0) {
-      j = border[j - 1];
-    } else {
-      i++;
+    while (i < n) {
+        if (text[i] == pattern[j]) {
+            if (j == m - 1) {
+                return i - m + 1;
+            }
+            i++;
+            j++;
+        } else if (j > 0) {
+            j = border[j - 1];
+        } else {
+            i++;
+        }
     }
-  }
-  return -1;
+    return -1;
 }
 
 /**
@@ -61,27 +61,27 @@ function kmpMatch(text, pattern) {
  *          for the given string at all indices
  */
 function calcKmpBorder(string) {
-  let border = new Array(string.length);
-  border[0] = 0;
+    let border = new Array(string.length);
+    border[0] = 0;
 
-  let m = string.length;
-  let j = 0;
-  let i = 1;
+    let m = string.length;
+    let j = 0;
+    let i = 1;
 
-  while (i < m) {
-    if (string[j] == string[i]) {
-      border[i] = j + 1;
-      i++;
-      j++;
-    } else if (j > 0) {
-      j = border[j - 1];
-    } else {
-      border[i] = 0;
-      i++;
+    while (i < m) {
+        if (string[j] == string[i]) {
+            border[i] = j + 1;
+            i++;
+            j++;
+        } else if (j > 0) {
+            j = border[j - 1];
+        } else {
+            border[i] = 0;
+            i++;
+        }
     }
-  }
 
-  return border;
+    return border;
 }
 
 /**
@@ -94,34 +94,34 @@ function calcKmpBorder(string) {
  *                  is not found
  */
 function bmMatch(text, pattern) {
-  let lastCharIndices = lastIndices(pattern);
+    let lastCharIndices = lastIndices(pattern);
 
-  let n = text.length;
-  let m = pattern.length;
-  let i = m - 1;
-  let j = m - 1;
+    let n = text.length;
+    let m = pattern.length;
+    let i = m - 1;
+    let j = m - 1;
 
-  if (i > n - 1) {
-    return -1;
-  }
-
-  do {
-    if (text[i] == pattern[j]) {
-      if (j == 0) {
-        return i;
-      } else {
-        i--;
-        j--;
-      }
-    } else {
-      let lastIndex = lastCharIndices[text[i]];
-      lastIndex = lastIndex == undefined ? -1 : lastIndex;
-      i = i + m - Math.min(j, 1 + lastIndex);
-      j = m - 1;
+    if (i > n - 1) {
+        return -1;
     }
-  } while (i <= n - 1);
 
-  return -1;
+    do {
+        if (text[i] == pattern[j]) {
+            if (j == 0) {
+                return i;
+            } else {
+                i--;
+                j--;
+            }
+        } else {
+            let lastIndex = lastCharIndices[text[i]];
+            lastIndex = lastIndex == undefined ? -1 : lastIndex;
+            i = i + m - Math.min(j, 1 + lastIndex);
+            j = m - 1;
+        }
+    } while (i <= n - 1);
+
+    return -1;
 }
 
 /**
@@ -135,13 +135,13 @@ function bmMatch(text, pattern) {
  *          within the given string
  */
 function lastIndices(string) {
-  let lastIndices = {};
+    let lastIndices = {};
 
-  for (let i = 0; i < string.length; i++) {
-    lastIndices[string[i]] = i;
-  }
+    for (let i = 0; i < string.length; i++) {
+        lastIndices[string[i]] = i;
+    }
 
-  return lastIndices;
+    return lastIndices;
 }
 
 /**
@@ -155,33 +155,33 @@ function lastIndices(string) {
  * @see {@link https://en.wikipedia.org/wiki/Levenshtein_distance#Iterative_with_full_matrix}
  */
 function levenshteinDistance(string1, string2) {
-  if (string1.length < string2.length) {
-    return levenshteinDistance(string2, string1);
-  }
-
-  let v0 = [];
-  let v1 = new Array(string2.length + 1);
-
-  for (let i = 0; i <= string2.length; i++) {
-    v0.push(i);
-  }
-
-  for (let i = 0; i < string1.length; i++) {
-    v1[0] = i + 1;
-
-    for (let j = 0; j < string2.length; j++) {
-      let deletionCost = v0[j + 1] + 1;
-      let insertionCost = v1[j] + 1;
-      let substitutionCost = string1[i] == string2[j] ? v0[j] : v0[j] + 1;
-      v1[j + 1] = Math.min(deletionCost, insertionCost, substitutionCost);
+    if (string1.length < string2.length) {
+        return levenshteinDistance(string2, string1);
     }
 
-    let v = v0;
-    v0 = v1;
-    v1 = v;
-  }
+    let v0 = [];
+    let v1 = new Array(string2.length + 1);
 
-  return v0[string2.length];
+    for (let i = 0; i <= string2.length; i++) {
+        v0.push(i);
+    }
+
+    for (let i = 0; i < string1.length; i++) {
+        v1[0] = i + 1;
+
+        for (let j = 0; j < string2.length; j++) {
+            let deletionCost = v0[j + 1] + 1;
+            let insertionCost = v1[j] + 1;
+            let substitutionCost = string1[i] == string2[j] ? v0[j] : v0[j] + 1;
+            v1[j + 1] = Math.min(deletionCost, insertionCost, substitutionCost);
+        }
+
+        let v = v0;
+        v0 = v1;
+        v1 = v;
+    }
+
+    return v0[string2.length];
 }
 
 module.exports = { match, kmpMatch, bmMatch, levenshteinDistance };
